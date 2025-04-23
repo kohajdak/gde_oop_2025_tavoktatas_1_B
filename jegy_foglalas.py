576a8e0387ce2ae8b01ed40a7a32447cedc4be92
kohajdak <kohajda.krisztian1209@gmail.com>
2025-04-23 21:02:38 +0200

    Foglalási rendszer frissítése: ID kezelés
    - Foglalási ID hozzáadása
    - Foglalás lemondásának módosítása
    - Hiba- és inputkezelés
    - Vizuális javítások

--- jegy_foglalas.py
+++ jegy_foglalas.py
@@ -3,15 +3,16 @@ class JegyFoglalas:
         self.foglalasok = []
 
     def jegy_foglalasa(self, jarat, utas_nev):
-        self.foglalasok.append({"utas": utas_nev, "jarat": jarat})
-        return f"\nFoglalás sikeres!\n\n{jarat.jarat_info()}" f"\nUtas: {utas_nev}"
+        foglalas_id = len(self.foglalasok) + 1
+        self.foglalasok.append({"id": foglalas_id, "utas": utas_nev, "jarat": jarat})
+        return f"\nFoglalás sikeres!\n\nFoglalás ID: {foglalas_id}\n{jarat.jarat_info()}" f"\nUtas: {utas_nev}"
 
-    def foglalas_lemondasa(self, utas_nev):
+    def foglalas_lemondasa(self, foglalas_id, utas_nev):
         for foglalas in self.foglalasok:
-            if foglalas["utas"] == utas_nev:
+            if foglalas["id"] == foglalas_id and foglalas["utas"] == utas_nev:
                 self.foglalasok.remove(foglalas)
-                return f"\nFoglalás lemondva!\n\n{foglalas['jarat'].jarat_info()}\nUtas: {utas_nev}"
-        return "Nincs ilyen foglalás!"
+                return f"\nFoglalás lemondva!\n\nFoglalás ID: {foglalas_id}\n{foglalas['jarat'].jarat_info()}\nUtas: {utas_nev}"
+        return "Hiba: Nincs ilyen foglalás az adott ID-val és névvel!"
 
     def foglalasok_listazasa(self):
         if not self.foglalasok:
@@ -19,6 +20,7 @@ class JegyFoglalas:
         else:
             output = "=" * 30 + "\n"
             for foglalas in self.foglalasok:
+                output += f"Foglalás ID: {foglalas['id']}\n"
                 output += f"Járat: {foglalas['jarat'].jarat_info()}\n"
                 output += f"Utas: {foglalas['utas']}\n"
                 output += "-" * 30 + "\n"
--- kezeles.py
+++ kezeles.py
@@ -1,7 +1,9 @@
 from menu import menu_print, get_valid_menu_input
+from utility import clear_screen
 
 
 def jaratok_listazasa(legi_tarsasag):
+    clear_screen()
     print("Elérhető járatok:")
     print("=" * 30 + "")
     for jarat in legi_tarsasag.jaratok:
@@ -27,16 +29,22 @@ def foglalas(jegy_foglalas, legi_tarsasag):
 
 def foglalasok_kezelese(jegy_foglalas):
     while True:
+        clear_screen()
         menu_print("manage")
         menu_selected = get_valid_menu_input([0, 1, 2], "\nVálasszon ki egy menüpontot!\n> ", "manage")
 
         if menu_selected == 1:
+            clear_screen()
             print("Aktív foglalások:")
             print(jegy_foglalas.foglalasok_listazasa())
             input("\nA folytatáshoz nyomjon meg egy billentyűt...")
         elif menu_selected == 2:
-            utas_nev = input("Adja meg az utas nevét a foglalás lemondásához: ")
-            print(jegy_foglalas.foglalas_lemondasa(utas_nev))
+            try:
+                foglalas_id = int(input("Adja meg a lemondani kívánt foglalás ID-jét: "))
+                utas_nev = input("Adja meg az utas nevét: ")
+                print(jegy_foglalas.foglalas_lemondasa(foglalas_id, utas_nev))
+            except ValueError:
+                print("Hiba: Az ID csak szám lehet!")
             input("\nA folytatáshoz nyomjon meg egy billentyűt...")
         elif menu_selected == 0:
             break
